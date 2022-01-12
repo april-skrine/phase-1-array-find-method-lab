2cd3ff1fd61861d9fe688894680609e99039ecbe
April Skrine <april.skrine@gmail.com>
2022-01-12 16:20:05 -0600

    commit tree be9a7158cc7758a451daa5ed4597690c98c43ff3

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function superbowlWin(someArray) {
+    let victories = someArray.find( ({ result }) => result === 'W');
+    if (victories === undefined) {
+        return undefined;
+    }
+    return victories.year;
+}
